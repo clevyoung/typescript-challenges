4c7aabeabab8e9e7632e43b68b0285f5ade1ac17
clevyoung <clevyoung@gmail.com>
2022-07-10 18:14:00 +0900

    파일명 변경

--- 4_easy_Tuple_to_Object.ts
+++ 4_easy_Tuple_to_Object.ts
@@ -9,3 +9,10 @@ type TupleToObject<T extends readonly (string | number | symbol)[]> = {
 const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const;
 
 type result = TupleToObject<typeof tuple>; // expected { tesla: 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+
+const tupleToObject: result = {
+  tesla: 'tesla',
+  'model 3': 'model 3',
+  'model X': 'model X',
+  'model Y': 'model Y',
+};
